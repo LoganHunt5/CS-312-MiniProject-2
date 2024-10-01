f38560cb77f90c2d580bd1ec393d080055e3e620
Logan <l.b.hunt5@gmail.com>
2024-09-30 21:20:20 -0700

    work

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from 'path';
+import axios from 'axios';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
+var emoji = "none";
+var emojidata;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/Public'));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {emojimap: EmojiMap, emoji: emoji});
+});
+
+app.get("/emoji", (req, res) => {
+    res.render("emoji.ejs", {emoji: emojidata.data});
+});
+
+app.post("/generate", async (req, res) => {
+    try{
+        emoji = [req.body["category"], req.body["type"]];
+        if(req.body["type"] === "Pick Category First"){
+            res.redirect("/");
+        } else if(req.body["category"] === "random") {
+            emojidata = await axios.get("https://emojihub.yurace.pro/api/random");
+        } else if (req.body["type"] !== "any") {
+            emojidata = await axios.get("https://emojihub.yurace.pro/api/random/category/" + emoji[0]);
+        } else {
+            emojidata = await axios.get("https://emojihub.yurace.pro/api/random/type/" + emoji[1]);
+        }
+        console.log(emojidata);
+        res.redirect("/emoji");
+    } catch(error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+
 });
 
 app.listen(port, () => {
@@ -20,3 +48,4 @@ app.listen(port, () => {
 });
 
 
+const EmojiMap = ["random", "smileys-and-people", "animals-and-nature","food-and-drink", "travel-and-places", "activites", "objects", "symbols", "flags"];
